f9e2b4275956bc37de250fdc6d9d0742e7bd4fb0
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-11-09 10:07:56 +0300

    clear

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,10 @@
 "use client";
-import { useEffect } from "react";
-import { getAPI } from "../../services/fetchAPI";
 import Image from "next/image";
 import Todo from "../components/todo";
-import { useRouter } from "next/navigation";
 import Todoform from "@/components/todoform";
 import Todolist from "@/components/todolist";
 
 export default function Home() {
-  useEffect(() => {
-    // async function test() {
-    //   const res = await fetch("api/todos");
-    //   const data = await res.json();
-    //   console.log(data, "data");
-    // }
-    // test();
-
-    async function test() {
-      const data = await getAPI("/todos");
-      console.log(data);
-    }
-    test();
-  }, []);
-
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Image
