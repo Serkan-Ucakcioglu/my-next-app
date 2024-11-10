0f34683d77da8981770214aef711ac59a030aaf9
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-11-10 12:44:56 +0300

    update img

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center  min-h-screen p-8 pb-20 gap-6 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Image
-        src={
-          "https://cdn.pixabay.com/photo/2020/01/21/18/39/todo-4783676_1280.png"
-        }
-        alt="logo"
+        src="https://cdn.pixabay.com/photo/2020/01/21/18/39/todo-4783676_1280.png"
+        alt="Todo Image"
         width={500}
         height={500}
+        layout="responsive"
+        priority
       />
 
       <Todoform />
