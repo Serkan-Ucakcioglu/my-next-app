d9956185001d3bd16dd87bfed931b56354672bee
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-11-10 14:09:33 +0300

    up

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
       },
       {
         // matching all API routes
-        source: "/api/todos/:path*",
+        source: "/api/todos/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
