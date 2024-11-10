8e1102bb8b04e461c861faacd122bc64350a608c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-11-10 13:28:18 +0300

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,19 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "/api/:path*",
+        source: "/api/v1/:slug",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
         ],
       },
