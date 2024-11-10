82e326337f48e297f5b4a5cea7617512a2eb902c
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2024-11-10 23:55:17 +0300

    update

--- src/components/todoform.jsx
+++ src/components/todoform.jsx
@@ -16,19 +16,29 @@ function Todoform() {
     setVal("");
   };
 
+  const enterSubmit = async (e) => {
+    console.log("test");
+
+    if (e.key == "Enter" && val.length > 4) {
+      console.log("test2");
+
+      await onsubmit();
+    }
+  };
+
   return (
     <div className="flex flex-col w-[500px]">
       <div className="flex items-center gap-2 ">
         <input
           value={val}
           onChange={(e) => setVal(e.target.value)}
-          onKeyDown={() => val < 4 && onsubmit()}
+          onKeyDown={(e) => enterSubmit(e)}
           className="w-full px-4 py-2 border rounded border-2 outline-none"
           type="text"
         />
         <button
           onClick={onsubmit}
-          disabled={val < 4}
+          disabled={val.length < 4}
           className="bg-primary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-25"
         >
           +
